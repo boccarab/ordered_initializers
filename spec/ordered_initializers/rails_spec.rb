007b69cae38d5c4c4ea6442537d4d21af1678089
Benjamin BOCCARA <boccarab@gmail.com>
2023-10-04 14:13:36 +0200

    Make path a class method

--- spec/ordered_initializers/rails_spec.rb
+++ spec/ordered_initializers/rails_spec.rb
@@ -19,24 +19,41 @@ describe OrderedInitializers::Railtie do
   before do
     Rails.env = "test"
     allow(Rails).to receive(:root).and_return Pathname.new(File.expand_path("../../fixtures", __FILE__))
-    allow_any_instance_of(OrderedInitializers::Parser).to receive(:path).and_return('initializers.yml')
 
     Rails.application = double(:application)
-    # Spring.watcher = SpecWatcher.new
   end
 
   after do
-    # Reset
-    # Spring.watcher = nil
     Rails.application = nil
   end
 
   context "before_initialize" do
-    it "calls #load_initializer_file" do
-      expect(OrderedInitializers::Railtie.instance).to receive(:load_initializer_file)
-      expect(OrderedInitializers).to receive(:go)
+    before do
+      allow(OrderedInitializers::Parser).to receive(:path).and_return(path)
+    end
 
-      ActiveSupport.run_load_hooks(:before_initialize)
+    context "when the initializers.yml file is present" do
+      let(:path) { 'initializers.yml' }
+
+      it "calls OrderedInitializer.go" do
+        expect(OrderedInitializers::Railtie.instance).to receive(:load_initializer_file).and_call_original
+        expect(OrderedInitializers).to receive(:go)
+
+        ActiveSupport.run_load_hooks(:before_initialize)
+      end
+    end
+
+    fcontext "when the initializers.yml file is missing" do
+      let(:path) { 'foobar.yml' }
+
+      it "does not call OrderedInitializer.go" do
+        expect(OrderedInitializers::Railtie.instance).to receive(:load_initializer_file).and_call_original
+        expect(OrderedInitializers).not_to receive(:go)
+
+        expect(Rails.logger).to receive(:info).with(/Skip ordered_initializers/)
+
+        ActiveSupport.run_load_hooks(:before_initialize)
+      end
     end
   end
 end
